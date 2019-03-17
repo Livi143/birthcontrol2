262cef04f0ebfc66e0ef74af28fb9883ccdc1379
erdakjo <ekenney60@gmail.com>
2019-03-17 15:32:07 -0600

    log changes

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -74,6 +74,9 @@ export default class Navigation extends Component {
                                                 <NavLink href="/profile">Profile</NavLink>
                                             </DropdownItem>
                                             <DropdownItem>
+                                                <NavLink href="/log">Daily Log</NavLink>
+                                            </DropdownItem>
+                                            <DropdownItem>
                                                 <NavLink onClick={this.logout}>Logout</NavLink>
                                             </DropdownItem>
                                         </>
