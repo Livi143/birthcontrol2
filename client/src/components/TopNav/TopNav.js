898bc51b53c24a6781ea650d1d282fad77a7687d
selinacore <ms.selinamaes@gmail.com>
2019-03-17 15:35:57 -0600

    added new icons

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -56,7 +56,7 @@ export default class Navigation extends Component {
         return (
             <div>
                 <Navbar className="navbar" light expand="md">
-                    <NavbarBrand href="/" className="titleFont"><i className="fas fa-key"></i> FlowSential</NavbarBrand>
+                    <NavbarBrand href="/" className="titleFont"><i className="fas fa-briefcase-medical"></i> FlowSential</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
